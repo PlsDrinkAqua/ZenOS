0dbf5b0ea967a279c0dd2bc3359925a4dd5a08d0
PlsDrinkAqua <oliver.liu233@gmail.com>
2025-03-30 16:00:36 +0100

    tested global constructor and added assembly codes to setup GDT

---
+++ kernel/kernel/global_constructors.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <kernel/tty.h>
+__attribute__((constructor)) 
+void foo(void){
+    terminal_initialize();
+    printf("foo is running and printf is available at this point\n");
+}
