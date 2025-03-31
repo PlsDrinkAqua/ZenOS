dfc14e587c45e8f0b813867286dd80d1dc45f832
PlsDrinkAqua <oliver.liu233@gmail.com>
2025-03-31 17:32:14 +0100

    added outb and inb function

---
+++ kernel/include/kernel/io.h
@@ -0,0 +1,10 @@
+#ifndef _IO_H
+#define _IO_H 1
+
+#include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t value);
+
+static inline uint8_t inb(uint16_t port);
+
+#endif
---
+++ kernel/kernel/IO/io.c
@@ -0,0 +1,12 @@
+#include <kernel/io.h>
+
+// Your inline assembly implementation of outb, inb, etc.
+static inline void outb(uint16_t port, uint8_t value) {
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t value;
+    asm volatile ("inb %w1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
