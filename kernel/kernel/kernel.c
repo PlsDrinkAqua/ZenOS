12290900bbec5bf8c81435d6d638d9b87ee3267c
PlsDrinkAqua <oliver.liu233@gmail.com>
2025-06-19 04:30:28 +0100

    Organize initilizing sequence

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,17 +4,38 @@
 #include <kernel/tty.h>
 #include <kernel/pic.h>
 #include <kernel/irq.h>
+#include <kernel/multiboot.h>
+#include <kernel/gdt.h>
+#include <kernel/idt.h>
 
 int debug = 4;
 
-void kernel_main(void) {
-	terminal_initialize();
+void kernel_main(multiboot_info_t* mbd, uint32_t magic) {
+    terminal_initialize();
+    printf("Terminal Initilized\n");
+
+    printf("Initilizing GDT.................");
+	gdt_install();
+	printf("done \n");
+
+	printf("Initilizing IDT.................");
+	idt_install();
+	printf("done \n");
+
+	printf("Initilizing Global Constructor.................");
+	__asm__ volatile ("call _init");
+	printf("done \n");
+
+	printf("Initilizing PIC.................");
 	PIC_remap(32, 40);
+	printf("done \n");
 	// Enable the IRQ for the keyboard by clearing its mask.
 	// IRQ_clear_mask(0);
 
 	// Enable interrupts globally.
+	printf("Enabling interrupts.................");
 	__asm__ volatile ("sti");
+	printf("done \n");
 	printf("Hello, kernel World!\n");
 	while(1){
 
